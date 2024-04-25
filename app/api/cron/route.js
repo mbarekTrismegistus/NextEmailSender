6a14726856371fa4ed035cad424446369bb54688
HermesThemes <momoboogeyman2000@gmail.com>
2024-04-25 15:52:27 +0000

    email cron job

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -4,27 +4,27 @@ import { NextResponse } from 'next/server'
 import {getLocalTimeZone, parseDate, today} from "@internationalized/date";
 
 export async function GET() {
-    let date = today(getLocalTimeZone()).toDate()
-    let d = await prisma.schedule.findMany({
-        include: {
-            user: true
-        },
-        where: {
-            isSent: false,
-            date: date
-        }
-    })
-    console.log(date)
+    // let date = today(getLocalTimeZone()).toDate()
+    // let d = await prisma.schedule.findMany({
+    //     include: {
+    //         user: true
+    //     },
+    //     where: {
+    //         isSent: false,
+    //         date: date
+    //     }
+    // })
+    // console.log(date)
 
-    if(d){
-        d.map((e) => {
-            axios.post(`${process.env.BASE_URL}/api/sendSchedulemail`, { data : {
-                    user: e.user,
-                    recievers: e.recievers,
-                    html: e.html || undefined,
-                    template: e.template || undefined,
-            }})
-        })
-    }
+    //     d.map((e) => {
+    //         axios.post(`${process.env.BASE_URL}/api/sendSchedulemail`, { data : {
+    //                 user: e.user,
+    //                 recievers: e.recievers,
+    //                 html: e.html || undefined,
+    //                 template: e.template || undefined,
+    //         }})
+    //     })
+    console.log("run")
+
     return NextResponse.json({ message: "ok" });
 }
