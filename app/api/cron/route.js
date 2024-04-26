031102b1a3f82078e3df08ac18a17da086402059
HermesThemes <momoboogeyman2000@gmail.com>
2024-04-26 09:30:02 +0000

    email cron job

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -5,18 +5,17 @@ import {getLocalTimeZone, parseDate, today} from "@internationalized/date";
 import dynamic from 'next/dynamic';
 import { renderAsync } from '@react-email/render';
 import { Resend } from 'resend';
-import nodemailer from 'nodemailer'
 
 export async function GET() {
 
-    // const resend = new Resend('re_S9m5Yo8b_CDvXhJcrTEKMYWzHzjhte6zR');
+    const resend = new Resend('re_S9m5Yo8b_CDvXhJcrTEKMYWzHzjhte6zR');
 
-    // resend.emails.send({
-    // from: 'onboarding@resend.dev',
-    // to: 'momoboogeyman2000@gmail.com',
-    // subject: 'Hello World',
-    // html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
-    // });
+    resend.emails.send({
+    from: 'onboarding@resend.dev',
+    to: 'momoboogeyman2000@gmail.com',
+    subject: 'Hello World',
+    html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+    });
 
     // let date = today(getLocalTimeZone()).toDate()
     // let html
@@ -40,24 +39,28 @@ export async function GET() {
     // }
 
 
-    const transport = nodemailer.createTransport({
-        host: 'smtp.zoho.com',
-        port: 465,
-        secure: true, //ssl
-        auth: {
-            user:"mbarektalbi@zohomail.com",
-            pass: "JPqbkC1fg2AV"
-        }
-    });
+    // const transporter = nodemailer.createTransport({
+    //     name: 'smtp.gmail.com',
+    //     host: 'smtp.gmail.com',
+    //     port: 465,
+    //     secure: true,
+    //     auth: {
+    //         user: d[0].user.email,
+    //         pass: d[0].user.smptpass,
+    //     },
+    //     tls: {
+    //         ciphers:'SSLv3'
+    //     }
+    // });
 
-    const options = {
-        "from": 'mbarektalbi@zohomail.com',
-        "to": "momoboogeyman2000@gmail.com",
-        "subject": "helloo",
-        "html": "<h1>zoho email</h1>"
-    }
-
-    await transport.sendMail(options) 
+    // const options = {
+    //     "from": d[0].user.email,
+    //     "to": d[0].recievers,
+    //     "subject": "helloo",
+    //     "html": html
+    // }
+    // console.log(transporter)
+    // let result = await transporter.sendMail(options) 
 
     // await prisma.email.create({
     //     data: {
