735a6880628c325b71d53cf91958eba5d047316c
HermesThemes <momoboogeyman2000@gmail.com>
2024-04-26 19:32:25 +0000

    email cron job

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,15 +1,15 @@
 import prisma from '@/prisma/client'
 import axios from 'axios';
-import { NextResponse } from 'next/server'
 import {getLocalTimeZone, parseDate, today} from "@internationalized/date";
 // import dynamic from 'next/dynamic';
 import { renderAsync } from '@react-email/render';
 import { Resend } from 'resend';
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
+
 
 // export const runtime = 'edge'; 
 // export const dynamic = 'force-dynamic';
-export async function POST() {
+// export async function GET() {
 
 //     const resend = new Resend('re_dLFcqiHD_NNWssnCRgtwuj1SrYaGKbTEB');
 
@@ -19,76 +19,57 @@ export async function POST() {
     //     subject: 'Hello World',
     //     html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
     // });
-    const transporter = nodemailer.createTransport({
-        name: 'smtp.zoho.com',
-        host: 'smtp.zoho.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: "mbarektalbi@zohomail.com",
-            pass: "AKLYwFnwKRgG",
-        }
-    });
+    // const transporter = nodemailer.createTransport({
+    //     name: 'smtp.zoho.com',
+    //     host: 'smtp.zoho.com',
+    //     port: 465,
+    //     secure: true,
+    //     auth: {
+    //         user: "mbarektalbi@zohomail.com",
+    //         pass: "AKLYwFnwKRgG",
+    //     }
+    // });
 
-    const mailData = {
-        from: {
-            name: `mbarek`,
-            address: "mbarektalbi@zohomail.com",
-        },
-        replyTo: "mbarektalbi@zohomail.com",
-        to: "mbarektalbi@zohomail.com",
-        subject: `form message`,
-        text: "message",
-        html: `<h1>ZOHO</h1>`,
-    };
+    // const mailData = {
+    //     from: {
+    //         name: `mbarek`,
+    //         address: "mbarektalbi@zohomail.com",
+    //     },
+    //     replyTo: "mbarektalbi@zohomail.com",
+    //     to: "mbarektalbi@zohomail.com",
+    //     subject: `form message`,
+    //     text: "message",
+    //     html: `<h1>ZOHO</h1>`,
+    // };
 
-    await new Promise((resolve, reject) => {
-        // verify connection configuration
-        transporter.verify(function (error, success) {
-            if (error) {
-                console.log(error);
-                reject(error);
-            } else {
-                console.log("Server is ready to take our messages");
-                resolve(success);
-            }
-        });
-    });
+    // await new Promise((resolve, reject) => {
+    //     // verify connection configuration
+    //     transporter.verify(function (error, success) {
+    //         if (error) {
+    //             console.log(error);
+    //             reject(error);
+    //         } else {
+    //             console.log("Server is ready to take our messages");
+    //             resolve(success);
+    //         }
+    //     });
+    // });
 
-    await new Promise((resolve, reject) => {
-        // send mail
-        transporter.sendMail(mailData, (err, info) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-            } else {
-                console.log(info);
-                resolve(info);
-            }
-        });
-    });
+    // await new Promise((resolve, reject) => {
+    //     // send mail
+    //     transporter.sendMail(mailData, (err, info) => {
+    //         if (err) {
+    //             console.error(err);
+    //             reject(err);
+    //         } else {
+    //             console.log(info);
+    //             resolve(info);
+    //         }
+    //     });
+    // });
     
 
-    // let date = today(getLocalTimeZone()).toDate()
-    // let html
-
-    // let d = await prisma.schedule.findMany({
-    //     include: {
-    //         user: true
-    //     },
-    //     where: {
-    //         isSent: false
-    //     }
-    // })
-
-    // if(d[0].template){
-    //     const Email = dynamic(async() => await import(`../../../emails/${d[0].template}`))
-    //     html = await renderAsync(<Email/>)
-
-    // }
-    // else if(d[0].html){
-    //     html = d[0].html
-    // }
+   
 
 
    
@@ -116,43 +97,57 @@ export async function POST() {
     
     
 
-    // let result = await axios.post(`${process.env.BASE_URL}/api/sendSchedulemail`, { data : {
-    //     user: d[0].user,
-    //     recievers: d[0].recievers,
-    //     html: d[0].html || undefined,
-    //     template: d[0].template || undefined,
-    // }})
-    return NextResponse.json({ message: "ok" });
+    
 // }
 
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 // export const runtime = "edge";
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
-// const RESEND_API_KEY = "re_2XcUAh4k_LaTe5yeQzBDd5pZEA55JZpbp";
 
-// export async function GET() {
-//     let arr = [1,2,3]
-//     arr.forEach(async(e) => {
-//         await fetch("https://api.resend.com/emails", {
-//             method: "POST",
-//             headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${RESEND_API_KEY}`,
-//             },
-//             body: JSON.stringify({
-//             from: "Acme <onboarding@resend.dev>",
-//             to: ["momoboogeyman2000@gmail.com"],
-//             subject: "hello world",
-//             html: `${e} <strong>it works!</strong>`,
-//             }),
-//         });
-//     })
+export async function GET() {
+
+
+    let d = await prisma.schedule.findMany({
+        include: {
+            user: true
+        },
+        where: {
+            isSent: false
+        }
+    })
+
+
+    d.map(async(e) => {
+        // await fetch("https://api.resend.com/emails", {
+        //     method: "POST",
+        //     headers: {
+        //     "Content-Type": "application/json",
+        //     Authorization: `Bearer ${RESEND_API_KEY}`,
+        //     },
+        //     body: JSON.stringify({
+        //     from: "Acme <onboarding@resend.dev>",
+        //     to: ["momoboogeyman2000@gmail.com"],
+        //     subject: "hello world",
+        //     html: html,
+        //     }),
+        // });
+        // await axios.post(`${process.env.BASE_URL}/api/sendSchedulemail`, {
+        //     date:
+        // })
+        await fetch(`${process.env.BASE_URL}/api/sendSchedulemail`, {
+            method: "POST",
+            body: JSON.stringify({
+                data: e
+            })
+        })
+    })
+    
   
 
 
-//     // const data = await res.json();
-//     return NextResponse.json({message: "aok"});
+    // const data = await res.json();
+    return NextResponse.json({message: "aok"});
 
 }
--- app/api/sendSchedulemail/route.js
+++ app/api/sendSchedulemail/route.js
@@ -5,7 +5,7 @@ import prisma from '@/prisma/client';
 import dynamic from 'next/dynamic';
 import { renderAsync } from '@react-email/render';
 
-
+const RESEND_API_KEY = "re_2XcUAh4k_LaTe5yeQzBDd5pZEA55JZpbp";
 
 export async function POST(request) {
     
@@ -13,44 +13,61 @@ export async function POST(request) {
     let html
     if(data.data.template){
         const Email = dynamic(async() => await import(`../../../emails/${data.data.template}`))
-        html = await renderAsync(<Email/>)
+        html = await renderAsync(<Email/>, {
+            pretty: true
+        })
 
     }
     else if(data.data.html){
         html = data.data.html
     }
+    console.log(html)
 
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: data.data.user.email,
-            pass: data.data.user.smptpass,
-        },
-        tls: {
-            ciphers:'SSLv3'
-        }
-    });
+    // const transporter = nodemailer.createTransport({
+    //     host: 'smtp.gmail.com',
+    //     port: 465,
+    //     secure: true,
+    //     auth: {
+    //         user: data.data.user.email,
+    //         pass: data.data.user.smptpass,
+    //     },
+    //     tls: {
+    //         ciphers:'SSLv3'
+    //     }
+    // });
 
-    const options = {
-        "from": data.data.user.email,
-        "to": data.data.recievers,
-        "subject": "helloo",
-        "html": html
-    }
+    // const options = {
+    //     "from": data.data.user.email,
+    //     "to": data.data.recievers,
+    //     "subject": "helloo",
+    //     "html": html
+    // }
 
-    await prisma.email.create({
-        data: {
-            template: data.data.template || "Html",
-            sender: data.data.user.email,
-            recievers: data.data.recievers,
-            userId: data.data.user.id
-        }
-    })
+    await fetch("https://api.resend.com/emails", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${RESEND_API_KEY}`,
+            },
+            body: JSON.stringify({
+            from: "Acme <onboarding@resend.dev>",
+            to: ["momoboogeyman2000@gmail.com"],
+            subject: "hello world",
+            html: html,
+            }),
+        });
 
-    await transporter.sendMail(options)
+    // await prisma.email.create({
+    //     data: {
+    //         template: data.data.template || "Html",
+    //         sender: data.data.user.email,
+    //         recievers: data.data.recievers,
+    //         userId: data.data.user.id
+    //     }
+    // })
+
+    // await transporter.sendMail(options)
 
     return NextResponse.json({message: "ok sent"})
 
